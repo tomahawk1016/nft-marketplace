88e332463874ee2220f0ad87014a72a4ff471de3
andreas <andreas.larsen062410@gmail.com>
2026-02-17 22:54:21 +0800

    chore: other files added

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomicfoundation/hardhat-toolbox");
+const path = require("path");
+require('dotenv').config({ path: path.join(__dirname, '/.env') })
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  }
+};
